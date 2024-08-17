aebc7a708642b4cd1c236aa6173c6611f30b2d5d
geekyfocus <gidabdulharkiem27.com>
2024-08-17 18:29:05 +0100

    2024-08-17T18:29:05+01:00

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,99 +1,3 @@
-// import { fileURLToPath, FileUrlToPathOptions } from "url";
-// import path from "path";
-// import nodemailer from "nodemailer";
-// import pug from "pug";
-// import { convert, htmlToText } from "html-to-text";
-// import SMTPTransport from "nodemailer/lib/smtp-transport";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// class Email {
-//   to: string;
-//   from: string;
-//   url: string;
-//   firstName: string;
-//   constructor(
-//     public user: any,
-//     public originalUrl: string
-//   ) {
-//     console.log("message reach here");
-//     console.log(user.email);
-//     this.to = user.email;
-//     this.from = ` Abdulhakeem  < ${process.env.EMAIL_FROM}>`;
-//     this.url = originalUrl;
-//     this.firstName = user.name.split(" ")[0];
-//   }
-//   newTransport() {
-//     if (process.env.NODE_ENV === "production") {
-//       const transporter = nodemailer.createTransport({
-//         service: "smtp.gmail.com",
-//         port: 465,
-//         auth: {
-//           user: process.env.EMAIL_FROM,
-//           pass: process.env.EMAIL_PASSWORD,
-//         },
-//       });
-//       console.log(transporter);
-//       return transporter;
-//     }
-//     // Explictly define
-//     const devTransportOps: SMTPTransport.Options = {
-//       host: process.env.EMAIL_HOST,
-//       port: parseInt(process.env.EMAIL_PORT as string, 10),
-//       auth: {
-//         user: process.env.EMAIL_FROM,
-//         pass: process.env.EMAIL_PASSWORD,
-//       },
-//       tls: {
-//         rejectUnauthorized: false,
-//       },
-//     };
-//     console.log(devTransportOps, "mail ops is available");
-//     const node = nodemailer.createTransport(devTransportOps);
-//     console.log(node, "mail node is available");
-//     return node;
-//   }
-//   async send(template: any, subject: any) {
-//     //pug template rendering
-//     console.log("message reach template");
-//     const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
-//       firstName: this.firstName,
-//       url: this.url,
-//       title: subject,
-//     });
-
-//     // mailOptions
-//     const mailOptions = {
-//       from: this.from,
-//       to: this.to,
-//       subject: subject,
-//       html,
-//       text: convert(html),
-//     };
-//     console.log(mailOptions, "mailOptions");
-//     const mailRes = await this.newTransport().sendMail(mailOptions);
-
-//     console.log(mailRes, "MailRes");
-//   }
-//   async sendWelcome() {
-//     await this.send("welcome", "Welcome to focusMetric!");
-//   }
-//   async sendResetPassword() {
-//     await this.send(
-//       "passwordReset",
-//       "Your password reset token  is only valid for 10 minutes!"
-//     );
-//   }
-//   async sendVerificationEmail() {
-//     console.log("message reach verification");
-//     const mail = await this.send("verifyUser", "Verify your email address");
-//     console.log(mail, "sheduled email");
-//     return mail;
-//   }
-// }
-
-// export default Email;
 import path from "path";
 import { fileURLToPath } from "url";
 import nodemailer from "nodemailer";
@@ -118,7 +22,7 @@ export default class Email {
   newTransport() {
     if (process.env.NODE_ENV === "production") {
       const transporter = nodemailer.createTransport({
-        service: "smtp.gmail.com",
+        service: "gmail",
         port: 465,
         auth: {
           user: process.env.EMAIL_FROM,
